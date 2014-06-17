9d5dbd86be0aa73c58e606a89849d4ba358c5f27
Pedro Martinez <pjmartinezgarcia@ucdavis.edu>
2014-06-17 11:49:06 -0700

    Addes coment before ggplot

--- plot_inflamation.r
+++ plot_inflamation.r
@@ -18,6 +18,7 @@ data.in <- read.csv(options[1])
 # melt with defaults
 data.in.melted <- melt(data.in)
  
+#CREATE A PLOT WITH GGPLOT2
 out.plot <- ggplot(data.in.melted,(aes(x=PatientID, y=value))) + 
     geom_point() + 
     stat_sum() +
